10f87663e882736a76e6c40988d435698b314a6c
pc <vanshikakhandelwal102@gmail.com>
2026-08-27 15:58:43 +0530

    Task2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
 
 import App from "./App";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import ProtectedRoute from "./components/ProtectedRoute";
-
 import { AuthProvider } from "./context/AuthContext";
 
 import "./index.css";
@@ -20,46 +10,8 @@ ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-
-          {/* =========================
-              PUBLIC ROUTES
-          ========================= */}
-
-          <Route
-            path="/login"
-            element={<Login />}
-          />
-
-          <Route
-            path="/register"
-            element={<Register />}
-          />
-
-          {/* =========================
-              PROTECTED ROUTES
-          ========================= */}
-
-          <Route element={<ProtectedRoute />}>
-            <Route
-              path="/"
-              element={<App />}
-            />
-          </Route>
-
-          {/* =========================
-              UNKNOWN ROUTE
-          ========================= */}
-
-          <Route
-            path="*"
-            element={<Navigate to="/" replace />}
-          />
-
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
